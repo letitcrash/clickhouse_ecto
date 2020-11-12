08dbf6a9d9d377b9b084aa4fec62e2e3b6f49058
Paul Small <pavelsmal@gmail.com>
2020-11-12 15:28:42 +0200

    add column rename support

--- lib/clickhouse_ecto/migration.ex
+++ lib/clickhouse_ecto/migration.ex
@@ -68,8 +68,16 @@ defmodule ClickhouseEcto.Migration do
   end
 
   def execute_ddl({:rename, %Table{} = _table, _current_column, _new_column}) do
-    # https://github.com/yandex/ClickHouse/issues/146
-    raise "It seems like reneaming columns is not supported..."
+    [
+      [
+        "ALTER TABLE ",
+        quote_table(table.prefix, table.name),
+        " RENAME COLUMN ",
+        quote_name(current_column),
+        " TO ",
+        quote_name(new_column)
+      ]
+    ]
   end
 
   def execute_ddl(string) when is_binary(string), do: [string]
